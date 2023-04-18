ad2441aa09aca3c09acfec0950e286327d4e72d1
pratik-img <120035831+pratik-img@users.noreply.github.com>
2023-04-18 23:16:05 +0100

    Created using Colaboratory

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyPtVhzMVq+KMp3rdGl0Bik9",
+      "authorship_tag": "ABX9TyOZ9xjBRgizgLe89JbWS9PA",
       "include_colab_link": true
     },
     "kernelspec": {
@@ -37,7 +37,7 @@
         "import requests\n",
         "import pandas as pd\n",
         "\n",
-        "url = 'https://dde-api.data.imgarena.com/soccer/fixtures?subscribed=true&&dateFrom=2023-04-19&dateTo=2023-04-19&type=official'\n",
+        "url = 'https://dde-api.data.imgarena.com/soccer/fixtures?subscribed=true&dateFrom=2023-04-19&dateTo=2023-04-19&type=official'\n",
         "headers = {\n",
         "    'Authorization': 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NGIwZmI2Ny0zNDVjLTRlMzctYjU1ZS05OGMxY2E1MmE3ZWUiLCJleHAiOiIyMDIzLTA1LTAyVDE0OjE3OjIxLjc5NzU1OCIsImlhdCI6IjIwMjMtMDQtMThUMTQ6MTc6MjEuNzk3NTU4Iiwic3ViIjoicHJhdGlrLnNoZXR0eUBpbWdhcmVuYS5jb20iLCJyb2xlIjp7InJvbGVOYW1lIjoiQWRtaW4ifX0.0bi61SWREdE0Q5OZajZioQWP4GGK7_JjZbFLiFLBnNxWs27EyPqH0rXwE4XndhUDne2R5j4H4A14ty2CqRK8TQ',\n",
         "    'Accept': 'text/csv, application/json, text/plain, application/vnd.imggaming.dde.api+json;version=1'\n",
